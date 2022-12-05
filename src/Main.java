0f9ba2021189ff7e0869d588e99709f9a00e454c
Vadim Kolonkov <smartmgn@mail.ru>
2022-12-05 20:14:51 +0500

    Расчет размера папок

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        String filePath = "C:/Users/User/Desktop/Дом";
+        File file = new File(filePath);
 
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if(folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
